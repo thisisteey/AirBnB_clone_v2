38a3e0851eef170e9f466f1f6f5123c6514990d0
thisisteey <dadataiwo82@yahoo.com>
2024-03-09 04:59:27 -0800

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """
 Module for web application deployment using Fabric
 """
-from os.path import exists
+from os.path import *
 from fabric.api import *
 
 env.hosts = ["100.25.199.90", "52.3.242.186"]
@@ -14,25 +14,27 @@ def do_deploy(archive_path):
     """Distributes archives to the web servers
     Args:
     archive_path: the path to the static files"""
-    try:
-        if not exists(archive_path):
-            print("File does not exist")
-            return False
-        archflname = archive_path.split("/")[-1].split('.')[0]
-        put(archive_path, "/tmp/")
-        fldpath = f"/data/web_static/releases/{archflname}"
-        tmppath = f"/tmp/{archflname}"
+    if not exists(archive_path):
+        return False
 
-        run(f"mkdir -p {fldpath}")
-        run(f"tar -xvzf {tmppath}.tgz -C {fldpath}")
-        run(f"rm {tmppath}.tgz")
-        run(f"mv {fldpath}/web_static/* {fldpath}")
-        run(f"rm -rf {fldpath}")
-        run(f"rm -rf /data/web_static/current")
-        run(f"ln -sf {fldpath} /data/web_static/current")
+    fldpath = splitext(archive_path)[0]
+    fldpath = fldpath.split('/')[-1]
+    flname = fldpath + '.tgz'
+
+    try:
+        put(archive_path, "/tmp/")
+
+        run(f'mkdir -p /data/web_static/releases/{fldpath}')
+        run(f'tar -xzf /tmp/{flname} -C /data/web_static/releases/{fldpath}')
+        run(f'rm /tmp/{flname}')
+        run(f'mv /data/web_static/releases/{fldpath}/web_static/*'
+            f' /data/web_static/releases/{fldpath}')
+        run(f'rm -rf /data/web_static/releases/{fldpath}/web_static')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -sf /data/web_static/releases/{fldpath} /data/web_static/current')
         run("sudo service nginx restart")
         print("New version deployed!")
         return True
-    except Exception as err:
-        print(f"Error: {err}")
+
+    except Exception:
         return False
