18855229fbc7990b7143c8b47d431d4ffa3d6aef
thisisteey <dadataiwo82@yahoo.com>
2024-03-09 12:11:40 -0800

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,8 @@
 """
 Module for web application deployment using Fabric
 """
-from os.path import *
+from os.path import exists, basename
+from datetime import datetime
 from fabric.api import *
 
 env.hosts = ["100.25.199.90", "52.3.242.186"]
@@ -10,32 +11,43 @@ env.user = "ubuntu"
 env.key_filename = "~/.ssh/school"
 
 
+def do_pack():
+    """Creates an archive of the static files"""
+    if not os.path.exists("versions"):
+        os.mkdir("versions")
+    currdt = datetime.utcnow()
+    result = f"versions/web_static_{currdt.strftime('%Y%m%d%H%M%S')}.tgz"
+    try:
+        print(f"Packing web_static to {result}")
+        local(f"tar -cvzf {result} web_static")
+        file_size = os.path.getsize(result)
+        print(f"web_static packed: {result} -> {file_size} Bytes")
+    except Exception as e:
+        print(f"Error: {e}")
+        result = None
+    return result
+
+
+@task(default=True)
 def do_deploy(archive_path):
     """Distributes archives to the web servers
     Args:
     archive_path: the path to the static files"""
     if not exists(archive_path):
         return False
-
-    fldpath = splitext(archive_path)[0]
-    fldpath = fldpath.split('/')[-1]
-    flname = fldpath + '.tgz'
-
     try:
         put(archive_path, "/tmp/")
-
-        run(f'mkdir -p /data/web_static/releases/{fldpath}')
-        run(f'tar -xzf /tmp/{flname} -C /data/web_static/releases/{fldpath}')
-        run(f'rm /tmp/{flname}')
-        run(f'mv /data/web_static/releases/{fldpath}/web_static/*'
-            f' /data/web_static/releases/{fldpath}')
-        run(f'rm -rf /data/web_static/releases/{fldpath}/web_static')
-        run('rm -rf /data/web_static/current')
-        run(f'ln -sf /data/web_static/releases/{fldpath}'
-            f' /data/web_static/current')
-        run("sudo service nginx restart")
+        flname = basename(archive_path)
+        flnamext = flname.split(".")[0]
+        fldpath = f"/data/web_static/releases/{flnamext}/"
+        sudo(f"mkdir -p {fldpath}")
+        sudo(f"tar -xzf /tmp/{flname} -C {fldpath}")
+        sudo(f"rsync -a {fldpath}web_static/ {fldpath}")
+        sudo(f"rm -rf {fldpath}web_static")
+        sudo(f"rm -rf /tmp/{flname}")
+        sudo(f"rm -rf /data/web_static/current")
+        sudo(f"ln -s {fldpath} /data/web_static/current")
         print("New version deployed!")
         return True
-
     except Exception:
         return False
