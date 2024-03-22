038522c02fa32031dd78e93bc1be3150ac04c783
thisisteey <dadataiwo82@yahoo.com>
2024-03-22 05:05:53 -0700

    Python script that starts a Flask web application listening on 0.0.0.0, port 5000 and display 'HBNB' and 'C' ollowed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+"""Flask application instance is defined"""
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """The home page message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb_page():
+    """The HBNB page message"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_page(text):
+    """The C page message displayed by the value of <text>"""
+    text = text.replace("-", " ")
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
