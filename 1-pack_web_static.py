4c4969bf525879b13dbd4cf0e6718360e0b69cc8
Gabriel Daramola <gabamazing21@gmail.com>
2024-10-27 06:02:42 +0100

    okay

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-"""
-Module for web application deployment using Fabric
-"""
-import os
+""" Module for compressing content"""
 from datetime import datetime
-from fabric.api import local, runs_once
+from fabric.api import local
+import os
 
 
-@runs_once
 def do_pack():
-    """Creates an archive of the static files"""
+    """Creating .tgz archive from the contents of the web static folder"""
     if not os.path.exists("versions"):
-        os.mkdir("versions")
-    currdt = datetime.utcnow()
-    result = f"versions/web_static_{currdt.strftime('%Y%m%d%H%M%S')}.tgz"
-    try:
-        print(f"Packing web_static to {result}")
-        local(f"tar -cvzf {result} web_static")
-        file_size = os.path.getsize(result)
-        print(f"web_static packed: {result} -> {file_size} Bytes")
-    except Exception as e:
-        print(f"Error: {e}")
-        result = None
-    return result
+        os.makedirs("versions")
+    #generating timestamp
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = f"versions/web_static_{timestamp}.tgz"
+
+    print(f"Packing web_static to {archive_name}")
+
+    #use the local command to create the .tgz archive
+
+    command = f"tar -cvzf {archive_name} web_static"
+    result = local(command)
+
+    # check if the archive was successfully created
+    if result.succeded:
+        print(f"web_static packed: {archive_name}")
+        return archive_name
+    else:
+        print("Failed to create the archive.")
+        return None
